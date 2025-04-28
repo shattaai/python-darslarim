8cd35207314967df229cc54a5ba38d30742870c5
Zafar Shonazarov <shonazarovzafar1@gmail.com>
2025-04-28 22:10:56 +0500

    yangi fayl qo'shildi
    for_tsikli haqida dars joylandi

---
+++ for_sikli_haqida/for_tsikli.py
@@ -0,0 +1,24 @@
+# mehmonlar = ["Ali", "Vali","Hasan","Husan"]
+# for mehmon in mehmonlar:
+#     print("Salom", mehmon)
+
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+
+# sonlar = list(range(11))
+# sonlar_kvadrati = []
+# for son in sonlar:
+#     sonlar_kvadrati.append(son**2)
+
+# print(sonlar)
+# print(sonlar_kvadrati)
+
+
+# dostlar = []
+# print("5ta eng yaqin do'stingizni ismini kiriting")
+# for n in range(5):
+#     dostlar.append(input(f"{n+1}- do'stingizni ismini kiriting"))
+#     print(dostlar)
